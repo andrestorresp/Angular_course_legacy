6dfca2ba40acc0536e47767861fbb15854da3862
andrestorresp <dragotorres16@gmail.com>
2024-04-09 16:30:29 -0400

    video 56

--- pruebaAngular/src/app/app.module.ts
+++ pruebaAngular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
 
