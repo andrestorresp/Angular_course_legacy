75e7707bb09d557bf359539a7f731d9c35dde4c4
andrestorresp <dragotorres16@gmail.com>
2024-04-09 10:40:05 -0400

    boostrap 4

--- pruebaAngular/src/app/app.component.ts
+++ pruebaAngular/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pruebaAngular';
-  name = 'ahora si';
+  nombre = 'Andres';
+  apellido = 'Torres';
+
 }
