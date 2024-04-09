7a733761d61e307ec7945618cc133a17415b068f
andrestorresp <dragotorres16@gmail.com>
2024-04-09 15:32:22 -0400

    ngfor/ ngif

--- pruebaAngular/src/app/components/body/body.component.ts
+++ pruebaAngular/src/app/components/body/body.component.ts
@@ -5,5 +5,11 @@ import { Component} from '@angular/core';
     templateUrl: './body.component.html'
 })
  export class BodyComponent{
+    mostrar = true;
 
+    frase: any ={
+        mensaje: 'Soy Batman',
+        autor: 'batman'
+    }
+    personajes: string[]= ['batman', 'superman', 'spiderman', 'drStrange']
 }
